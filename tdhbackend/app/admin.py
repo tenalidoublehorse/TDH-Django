1f50248117378ce265762a13d741f5b3babe89bb
website-krysta <website.krysta@gmail.com>
2024-01-06 19:55:46 +0530

    download csv

--- tdhbackend/app/admin.py
+++ tdhbackend/app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import BlogPost,Banner,IndusData
 # Register your models here.
+
+import csv
+from django.http import HttpResponse
+
+
 class AdminBlogpost(admin.ModelAdmin):
     list_display=('id','image','title','shortdesc','postdate','authorname')
 
@@ -9,6 +14,18 @@ class AdminBanner(admin.ModelAdmin):
 
 class AdminIndusData(admin.ModelAdmin):
     list_display=('id','Firstname','Lastname','Email','Phonenumber','PurposeofContact','Message','AddedTimeStamp')
+    actions = ['export_to_csv']
+    def export_to_csv(self, request,queryset):
+        meta = self.model._meta
+        fieldnames = [field.name for field in meta.fields]
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment;filename=export.csv'
+        writer = csv.writer(response)
+        writer.writerow(fieldnames)
+        for obj in queryset:
+             writer.writerow([getattr(obj, field) for field in fieldnames])
+        return response
+    export_to_csv.short_description = "Download selected as csv"
 
 admin.site.register(BlogPost,AdminBlogpost)
 admin.site.register(Banner,AdminBanner)
